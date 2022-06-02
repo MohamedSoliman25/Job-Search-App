dc89311a9d12c2f76122c4c3baebf71431e19e35
Mohamed Soliman <mohamedelzmar2018@gmail.com>
2022-06-02 09:19:30 +0200

    design finished

--- app/src/main/java/com/example/jobsearchapp/MainActivity.kt
+++ app/src/main/java/com/example/jobsearchapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.jobsearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.jobsearchapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = ""
     }
 }
